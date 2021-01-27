b678a6c4830373554231a8347bf8d4e5985ac5dc
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-01-27 21:16:05 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -3,9 +3,9 @@ require 'pry'
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
    return "I LOVE YOU TOO PUMPKIN!"
- elsif phrase.downcase
+ elsif phrase.downcase == "#{phrase}"
     return "HUH?! SPEAK UP, SONNY!"
-  elsif phrase.upcase
+  elsif phrase.upcase == "#{phrase}"
     return "NO, NOT SINCE 1938!"
   end
 end
