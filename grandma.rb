77d00c0e04ec4fbcaf0780b309b357021a36fb03
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-01-27 20:43:00 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,9 +1,9 @@
 require 'pry'
 # Write a speak_to_grandma method.
 def speak_to_grandma(phrase)
-  #if phrase = "I LOVE YOU GRADMA!"
-  #  return "I LOVE YOU TOO PUMPKIN!"
-  if phrase == "Hi Nana, how are you?"
+  if phrase == "I LOVE YOU GRADMA!"
+   return "I LOVE YOU TOO PUMPKIN!"
+ elsif phrase == "Hi Nana, how are you?"
     return "HUH?! SPEAK UP, SONNY!"
   else phrase.upcase
     return "NO, NOT SINCE 1938!"
